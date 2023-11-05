70dab4270d2f56265c667ae4f60a08be6414dd82
SavindaJayasekara <thantrige32@gmail.com>
2023-11-05 09:59:34 +0530

    add style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,22 @@
 $('#readerNav').on('click',function (){
     $('#reader').css('display','block');
     $('#generator').css('display','none');
+    $('#readerNav').css({
+        color:'blue'
+    });
+    $('#genarate').css({
+        color:'black'
+    });
 });
 
 $('#genarate').on('click',function (){
     $('#reader').css('display','none');
     $('#generator').css('display','block');
+    $('#readerNav').css({
+        color:'black'
+    });
+
+    $('#genarate').css({
+        color:'blue'
+    });
 });
